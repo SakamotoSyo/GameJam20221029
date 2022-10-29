b6e464b910847c95f30f7ba32724652bc3dd951c
DRGO <abokado0901@gmail.com>
2022-10-29 14:40:47 +0900

    Gamemanagerのゲームオーバーメソッド、ゲームクリアメソッド、スコア加算メソッド(使わなくても良い)追加

--- Assets/InGame/Iwadare/GameManager.cs
+++ Assets/InGame/Iwadare/GameManager.cs
@@ -5,18 +5,67 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    [Tooltip("時間を表示するテキスト")]
     [SerializeField] Text _time;
+    [Tooltip("スコアを表示するテキスト")]
+    [SerializeField] Text _scoreText;
+    [Tooltip("時間")]
     [SerializeField] float _countDown = 100f;
-    [SerializeField] string _sceneLoadName;
+    [Tooltip("ゲームオーバーシーン(なかったらタイトルシーン)")]
+    [SerializeField] string _gameOverSceneName;
+    [Tooltip("ゲームクリアシーン")]
+    [SerializeField] string _gameCrearSceneName;
+    [Tooltip("タイムオーバーの際のテキスト表示")]
+    [SerializeField] GameObject _timeOverText;
+    [Tooltip("幽霊につかまった際のテキスト表示")]
+    [SerializeField] GameObject _catchPlayerText;
+    [Tooltip("脱出した際のテキスト表示")]
+    [SerializeField] GameObject _escapeText;
+    [Tooltip("スコア")]
+    int _score = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        AddScore(0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        _time.text = string.Format("{0:00.00}" ,_countDown);
+        if (_time)
+        {
+            _time.text = string.Format("{0:00.00}", _countDown);
+            _countDown = Mathf.Max(0f, _countDown - Time.deltaTime);
+        }
+        if(_countDown <= 0)
+        {
+            _timeOverText.SetActive(true);
+            IwadareSceneLoader.Instance.SceneLoad(_gameOverSceneName);
+        }
+        
+    }
+    /// <summary>スコアの追加のメソット</summary>
+    /// <param name="score"></param>
+    public void AddScore(int score)
+    {
+        _score += score;
+        _scoreText.text = string.Format("0000000", _score);
+    }
+
+    /// <summary>幽霊につかまった際のメソッド</summary>
+    public void GameOver()
+    {
+        _catchPlayerText.SetActive(true);
+        IwadareSceneLoader.Instance.SceneLoad(_gameOverSceneName);
+    }
+    /// <summary>脱出した際のメソッド</summary>
+    public void EscapeText()
+    {
+        _escapeText.SetActive(true);
+        IwadareSceneLoader.Instance.SceneLoad(_gameCrearSceneName);
+    }
+    private void OnLevelWasLoaded(int level)
+    {
+        //_countDown = 100f;
     }
 }
--- Assets/InGame/Iwadare/IwadareSceneLoader.cs
+++ Assets/InGame/Iwadare/IwadareSceneLoader.cs
@@ -3,23 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/// <summary>SceneLoaderのシングルトン</summary>
 public class IwadareSceneLoader : SingletonMonovihair<IwadareSceneLoader>
 {
     protected override bool _dontDestroyOnLoad { get{ return true; } }
-
+    /// <summary>アプリケーションの終了</summary>
     public void EndGame()
     {
         Application.Quit();
     }
-
+    /// <summary>シーンのロード(Coroutimeにより3秒の遅延)</summary>
+    /// <param name="_loadSceneName"></param>
     public void SceneLoad(string _loadSceneName)
     {
         StartCoroutine(SceneLoadStart(_loadSceneName));
     }
-    
+
     IEnumerator SceneLoadStart(string _loadSceneName)
     {
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(3f);
+        Debug.Log("GameOver");
         SceneManager.LoadScene(_loadSceneName);
     }
 }
